ab6dd69c8214baab57227e1f2eae7af98158dc02
soseen <lampard082@gmail.com>
2021-04-03 00:09:03 +0200

    initial

--- src/SearchInput.tsx
+++ src/SearchInput.tsx
@@ -2,7 +2,7 @@ import './SearchInput.scss'
 import { Button, Input} from 'reactstrap'
 import { Video } from './App'
 import { useState } from 'react';
-import searchYoutube from './axios';
+import { searchYoutube } from './axios';
 import { useQuery } from 'react-query';
 
 
@@ -51,8 +51,6 @@ type Props = {
 const SearchInput: React.FC<Props> = ({addVideo}) => {
 
     const [videoQuery, setVideoQuery] = useState<string>('');
-    const [videoId, setVideoId] = useState<string | null>(null);
-
 
     const getVideo = async (videoQuery: string): Promise<void> => {
 
@@ -63,15 +61,6 @@ const SearchInput: React.FC<Props> = ({addVideo}) => {
         })
 
         console.log(IdResponse.data.items[0].id.videoId);
-
-
-
-        // const videoResponse = await searchYoutube.get<SearchYoutubeResponse>('/search', {
-        //     params: {
-        //         q: videoQuery
-        //     }       
-        // })
-
 
     }
 
@@ -90,7 +79,7 @@ const SearchInput: React.FC<Props> = ({addVideo}) => {
                             type="email" 
                             name="email" 
                             className="input mr-10" 
-                            placeholder="video URL..." 
+                            placeholder="video URL" 
                             value={videoQuery} 
                             onChange={(e: React.FormEvent<HTMLInputElement>) => handleChange(e)}
                         />             
--- src/axios.js
+++ src/axios.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
 
-export default  axios.create({
+export const searchYoutube = axios.create({
     baseURL: 'https://youtube.googleapis.com/youtube/v3/',
     params: {
         part: 'snippet',
         maxResults: 1,
-        // key: process.env.YOUTUBE_API_KEY
-        key: 'AIzaSyC-ejC54WuaNvVJn-Bymq-taATjyCURQSU'
+        key: process.env.REACT_APP_YOUTUBE_API_KEY
     },
     headers: {}
-})
+})  
